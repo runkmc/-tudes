39dff1e4f60a22f0f28fb838bc9e17b022dee702
= <kevin.mcgladdery@gmail.com>
2015-12-24 09:28:50 -0800

    Complete 2-3

--- 02/elixir/geom.ex
+++ 02/elixir/geom.ex
@@ -1,4 +1,11 @@
 defmodule Geom do
+  @moduledoc """
+  Functions for calculation the area of shapes.
+  """
+
+  @doc """
+  Calculates the area of a rectangle
+  """
   def area(x \\ 1 ,y \\ 1) do
     x * y
   end
