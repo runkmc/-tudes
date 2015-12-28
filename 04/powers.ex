9d4d20bdcf3cc9e7c62195af9ae20ce26fd1c1bf
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-12-28 12:27:55 -0800

    Complete etude 4-5

--- 04/powers.ex
+++ 04/powers.ex
@@ -24,4 +24,20 @@ defmodule Powers do
     r(x, (n - 1), (x * acc))
   end
 
+  def nth_root(x, n) do
+    nth_root(x, n, x / 2.0)
+  end
+
+  defp nth_root(x, n, estimate) do
+    IO.puts "Current guess is #{estimate}."
+    f = r(estimate, n) - x
+    f_prime = n * r(estimate, n - 1)
+    next = estimate - (f / f_prime)
+    change = abs(next - estimate)
+    cond do
+      change < 1.0e-8 -> next
+      true            -> nth_root(x, n, next)
+    end
+  end
+
 end
