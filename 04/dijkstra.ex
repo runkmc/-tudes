a30a8855e7a5ec352d2334ef40bf41d6768a5aae
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-12-28 12:03:15 -0800

    Complete etude 4-2

---
+++ 04/dijkstra.ex
@@ -0,0 +1,15 @@
+defmodule Dijkstra do
+
+  def gcd(m, n) when m == n do
+    m
+  end
+
+  def gcd(m, n) when m > n do
+    gcd((m - n), n)
+  end
+
+  def gcd(m, n) do
+    gcd(m, (n - m))
+  end
+
+end
