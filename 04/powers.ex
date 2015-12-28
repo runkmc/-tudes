827865c996d006f47d432425453c011b1e2b4853
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2015-12-28 12:15:45 -0800

    Complete etude 4-4

--- 04/powers.ex
+++ 04/powers.ex
@@ -9,11 +9,19 @@ defmodule Powers do
   end
 
   def r(x, n) when n > 1 do
-    x * r(x, (n - 1))
+    r(x, n, 1)
   end
 
   def r(x, n) when n < 0 do
     1.0 / r(x, -n)
+  end
+
+  defp r(x, n, acc) when n == 0 do
+    acc
+  end
+  
+  defp r(x, n, acc) do
+    r(x, (n - 1), (x * acc))
   end
 
 end
